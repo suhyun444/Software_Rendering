340c22733f121047cd499e329451493d72ef0816
정수현 <windseed71@naver.com>
2023-01-31 20:18:01 +0900

    CheckInside, Evaluate 함수 접근 제한자 변경

--- Software_Rendering/EdgeEquation.h
+++ Software_Rendering/EdgeEquation.h
@@ -3,12 +3,11 @@
 
 class EdgeEquation
 {
-private:
-    bool CheckInside(float x, float y);
-    bool CheckInside(float v);
-    float Evaluate(float x, float y);
 public:
     float a,b,c;
     bool tie;
     EdgeEquation(const Vector3& v1, const Vector3& v2);
+    bool CheckInside(float x, float y);
+    bool CheckInside(float v);
+    float Evaluate(float x, float y);
 };
