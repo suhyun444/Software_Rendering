f9e66d501b5e3141a84b180638ab9890b5bcc3e0
정수현 <windseed71@naver.com>
2023-02-01 13:47:19 +0900

    삼각형 레스터화 구현 중

--- Software_Rendering/Draw.cpp
+++ Software_Rendering/Draw.cpp
@@ -8,7 +8,7 @@ void Draw::DrawTriangle(const HDC& hdc,const Vector3 &v1, const Vector3 &v2, con
     LineTo(hdc, v3.x, v3.y);
     MoveToEx(hdc, v3.x, v3.y, NULL);
     LineTo(hdc, v1.x, v1.y);
-    return;
+    //return;
     // Compute triangle bounding box.
     float minX = Math::Min<float>(Math::Min<float>(v1.x, v2.x), v3.x);
     float maxX = Math::Max<float>(Math::Max<float>(v1.x, v2.x), v3.x);
@@ -22,30 +22,27 @@ void Draw::DrawTriangle(const HDC& hdc,const Vector3 &v1, const Vector3 &v2, con
     maxY = Math::Min<float>(maxY, (float)Window::height);
 
     // Compute edge equations.
-    EdgeEquation e0(v2, v3);
-    EdgeEquation e1(v3, v1);
-    EdgeEquation e2(v1, v2);
+    EdgeEquation e1(v2, v3);
+    EdgeEquation e2(v3, v1);
+    EdgeEquation e3(v1, v2);
 
-    float area = 0.5 * (e0.c + e1.c + e2.c);
+    float area = 0.5 * (e1.c + e2.c + e3.c);
 
     // Check if triangle is backfacing.
     if (area < 0)
         return;
 
-    ParameterEquation r(v1.x, v2.x, v3.x, e0, e1, e2, area);
-    ParameterEquation g(v1.y, v2.y, v3.y, e0, e1, e2, area);
-    ParameterEquation b(v1.z, v2.z, v3.z, e0, e1, e2, area);
+    ParameterEquation r(v1.x, v2.x, v3.x, e1, e2, e3, area);
+    ParameterEquation g(v1.y, v2.y, v3.y, e1, e2, e3, area);
+    ParameterEquation b(v1.z, v2.z, v3.z, e1, e2, e3, area);
 
     // Add 0.5 to sample at pixel centers.
     for (float x = minX + 0.5f, xm = maxX + 0.5f; x <= xm; x += 1.0f)
         for (float y = minY + 0.5f, ym = maxY + 0.5f; y <= ym; y += 1.0f)
         {
-            if (e0.CheckInside(x, y) && e1.CheckInside(x, y) && e2.CheckInside(x, y))
+            if (e1.CheckInside(x, y) && e2.CheckInside(x, y) && e3.CheckInside(x, y))
             {
-                int rint = r.Evaluate(x, y) * 255;
-                int gint = g.Evaluate(x, y) * 255;
-                int bint = b.Evaluate(x, y) * 255;
-                SetPixel(hdc, x, y, RGB(rint, gint, bint));
+                SetPixel(hdc, x, y, RGB(0, 1, 1));
             }
         }
 }
