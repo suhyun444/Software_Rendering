0a3a32c9f4150a30334708f65e89cfcc3b08a697
정수현 <windseed71@naver.com>
2023-02-01 17:06:50 +0900

    Evaluate함수 수정

--- Software_Rendering/EdgeEquation.h
+++ Software_Rendering/EdgeEquation.h
@@ -6,6 +6,7 @@ class EdgeEquation
 public:
     float a,b,c;
     bool tie;
+    Vector3 v1,v2;
     EdgeEquation(const Vector3& v1, const Vector3& v2);
     bool CheckInside(float x, float y);
     bool CheckInside(float v);
