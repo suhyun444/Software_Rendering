9de1488b7f4235a7edeb1e066a915fe4405cbd70
정수현 <windseed71@naver.com>
2023-02-02 14:37:30 +0900

    프레임 레이트 추가

--- Software_Rendering/Time.cpp
+++ Software_Rendering/Time.cpp
@@ -4,6 +4,9 @@ INT64 Time::frequency = 0;
 float Time::ticksPerMs = 0;
 INT64 Time::startTime = 0;
 float Time::deltaTime = 0;
+int Time::frameCount = 0;
+int Time::curFrameCount = 0;
+float Time::frameTime = 0;
 
 bool Time::Initialize()
 {
@@ -25,7 +28,19 @@ void Time::Frame()
 	INT64 currentTime;
 	QueryPerformanceCounter((LARGE_INTEGER *)&currentTime);
 	startTime = currentTime;
+	CountFrame();
 	return;
+}
+void Time::CountFrame()
+{
+	frameTime += deltaTime;
+	curFrameCount++;
+	if (frameTime >= 1.0f)
+	{
+		frameTime = 0.0f;
+		frameCount = curFrameCount;
+		curFrameCount = 0;
+	}
 }
 void Time::Update()
 {
--- Software_Rendering/Time.h
+++ Software_Rendering/Time.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <iostream>
 
 class Time
 {
@@ -8,10 +9,19 @@ private:
 	static float ticksPerMs;
 	static INT64 startTime;
 	static float deltaTime;
+	static int frameCount;
+	static int curFrameCount;
+	static float frameTime;
 public:
 
 	static bool Initialize();
 	static void Frame();
+	static void CountFrame();
 	static void Update();
 	static float GetDeltaTime() {return deltaTime;}
+	static LPCTSTR GetFrameCount(char* buffer)
+	{
+		_itoa(frameCount, buffer, 10);
+		return (LPCTSTR)buffer;
+	}
 };
