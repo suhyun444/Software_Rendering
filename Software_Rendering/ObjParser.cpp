4d138429365bcd738f46da753ae31fd5ff1cea97
정수현 <windseed71@naver.com>
2023-02-13 23:07:38 +0900

    obj 파일 경로 변경

--- Software_Rendering/ObjParser.cpp
+++ Software_Rendering/ObjParser.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <string>
 
-Index FaceValueToIndex(std::string input1,std::string input2,std::string input3)
+Index FaceValueToVertexIndex(std::string input1,std::string input2,std::string input3)
 {
     std::string x, y, z;
     std::stringstream ss1(input1);
@@ -17,9 +17,26 @@ Index FaceValueToIndex(std::string input1,std::string input2,std::string input3)
     int zInt = std::stoi(z);
     return Index(xInt - 1,yInt - 1,zInt - 1);
 }
+Index FaceValueToTextureIndex(std::string input1, std::string input2, std::string input3)
+{
+    std::string x, y, z;
+    std::stringstream ss1(input1);
+    std::getline(ss1, x, '/');
+    std::getline(ss1, x, '/');
+    std::stringstream ss2(input2);
+    std::getline(ss2, y, '/');
+    std::getline(ss2, y, '/');
+    std::stringstream ss3(input3);
+    std::getline(ss3, z, '/');
+    std::getline(ss3, z, '/');
+    int xInt = std::stoi(x);
+    int yInt = std::stoi(y);
+    int zInt = std::stoi(z);
+    return Index(xInt - 1, yInt - 1, zInt - 1);
+}
 Object ObjParser::LoadObject(std::string objectName)
 {
-    std::ifstream fin("D:\\Winapi\\Software_Rendering\\Software_Rendering\\" + objectName);
+    std::ifstream fin("D:\\Winapi\\Software_Rendering\\Software_Rendering\\Objects\\" + objectName);
     std::string s;
     Object result;
     if(!fin)
@@ -36,13 +53,20 @@ Object ObjParser::LoadObject(std::string objectName)
             Vector3 v = Vector3(x, y, z);
             result.vertices.push_back(v);
         }
+        else if(s == "vt")
+        {
+            float x,y;
+            fin >> x >> y;
+            Vector2 v = Vector2(x,y);
+            result.textureVertices.push_back(v);
+        }
         else if(s == "f")
         {
             std::string input1, input2, input3;
             fin >> input1 >> input2 >> input3;
-            Index index = FaceValueToIndex(input1,input2,input3); 
-            Index i = index;
-            result.indices.push_back(i);
+            Index vertexIndex = FaceValueToVertexIndex(input1, input2, input3);
+            Index textureIndex = FaceValueToTextureIndex(input1,input2,input3);
+            result.indices.push_back(vertexIndex);
         }
     }
     result.InitVertexVector();
