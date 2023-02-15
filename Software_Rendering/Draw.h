75924095a992a02a787d2318f4f8d368e4819e6d
정수현 <windseed71@naver.com>
2023-02-15 18:06:27 +0900

    텍스쳐 매핑 추가

--- Software_Rendering/Draw.h
+++ Software_Rendering/Draw.h
@@ -2,6 +2,7 @@
 #include "Window.h"
 #include "BaryCenterCalculator.h"
 #include "PhongShader.h"
+#include "TextureMapping.h"
 #include "BitmapBuffer.h"
 
 class Draw
@@ -9,5 +10,5 @@ class Draw
 public:
     static void Update(const Vector3& cameraPosition) {PhongShader::CameraPosition = cameraPosition;}
     static void PrintText(const HDC& hdc,const int x,const int y, const LPCTSTR& text);
-    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, PhongShader& phongShader);
+    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, PhongShader &phongShader, TextureMapping &textureMapping);
 };
