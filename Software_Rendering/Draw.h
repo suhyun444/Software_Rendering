2b3ca34e115fa8573ccf44a47423fefaac486b8a
정수현 <windseed71@naver.com>
2023-03-05 19:55:43 +0900

    원근 투영 보정 추가

--- Software_Rendering/Draw.h
+++ Software_Rendering/Draw.h
@@ -10,5 +10,5 @@ class Draw
 public:
     static void Update(const Vector3& cameraPosition) {PhongShader::CameraPosition = cameraPosition;}
     static void PrintText(const HDC& hdc,const int x,const int y, const LPCTSTR& text);
-    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, PhongShader &phongShader, TextureMapping &textureMapping);
+    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3,float z1,float z2,float z3, PhongShader &phongShader, TextureMapping &textureMapping);
 };
