90c0a26e4be2da9fdebe9c7fa9bfb8c913a10146
정수현 <windseed71@naver.com>
2023-04-30 19:40:16 +0900

    Add vertexshader class for separate function

--- Software_Rendering/Object.cpp
+++ Software_Rendering/Object.cpp
@@ -29,25 +29,7 @@ Matrix4x4 Object::GetModelMatrix()
 void Object::UpdateTransform(const Camera &camera)
 {
     Matrix4x4 modelMatrix = Matrix4x4::GetModelMatrix(scale, rotation, position);
-    Matrix4x4 t = camera.GetProjectionMatrix() * camera.GetViewMatrix() * modelMatrix;
-    for (int i = 0; i < vertices.size(); i++)
-    {
-        Vector4 p = Vector4(vertices[i]);
-
-        Vector4 vWcs = modelMatrix * p;
-        worldCoordinateVertices[i] = Vector3(vWcs.x, vWcs.y, vWcs.z);
-        Vector4 vVcs = camera.GetViewMatrix() * vWcs;
-        zPositionInViewCoordinate[i] = vVcs.z;
-
-        Vector4 vNdcs = t * p;
-        // Clipping coordinate system -> Normalized device coordinate system
-        deviceCoordinateVertices[i] = Vector3(vNdcs.x / vNdcs.w, vNdcs.y / vNdcs.w, vNdcs.z / vNdcs.w);
-        // Normalized device coordinate system -> Device coordinate system
-        deviceCoordinateVertices[i].x += 1;
-        deviceCoordinateVertices[i].x *= (float)Window::width / 2.0f;
-        deviceCoordinateVertices[i].y += 1;
-        deviceCoordinateVertices[i].y *= (float)Window::height / 2.0f;
-    }
+    vertexShader.BindMatrix(modelMatrix,camera.GetViewMatrix(),camera.GetProjectionMatrix());
 }
 // https://velog.io/@jaehyeoksong0/graphics-3
 // TODO : BackfaceCulling 최적화하기
@@ -60,7 +42,11 @@ void Object::Draw(BitmapBuffer &bitmapBuffer, const Camera &camera)
     Vector3 viewingVector = position - camera.position;
     for (int i = 0; i < indices.size(); i++)
     {
-        Vector3 v1 = worldCoordinateVertices[indices[i]._1], v2 = worldCoordinateVertices[indices[i]._2], v3 = worldCoordinateVertices[indices[i]._3];
+        Vector3 v[3] = {vertices[indices[i]._1], vertices[indices[i]._2], vertices[indices[i]._3]};
+        Vector2 vt[3] = {textureVertices[textureIndices[i]._1], textureVertices[textureIndices[i]._2], textureVertices[textureIndices[i]._3]};
+        Output output = vertexShader.Calculate(v,vt);
+
+        Vector3 v1 = output.worldCoordinateVertex[0], v2 = output.worldCoordinateVertex[1], v3 = output.worldCoordinateVertex[2];
         Vector3 s1 = v2 - v1;
         Vector3 s2 = v3 - v1;
         Vector3 normal = s1.Cross(s2);
@@ -69,18 +55,19 @@ void Object::Draw(BitmapBuffer &bitmapBuffer, const Camera &camera)
         // back-face culling
         if (DontNeedToDraw(position - camera.position, normal))
             continue;
-        Vector3 p1 = deviceCoordinateVertices[indices[i]._1];
-        Vector3 p2 = deviceCoordinateVertices[indices[i]._2];
-        Vector3 p3 = deviceCoordinateVertices[indices[i]._3];
 
-        Vector2 vt1 = textureVertices[textureIndices[i]._1];
-        Vector2 vt2 = textureVertices[textureIndices[i]._2];
-        Vector2 vt3 = textureVertices[textureIndices[i]._3];
+        Vector3 p1 = output.deviceCoordinateVectex[0];
+        Vector3 p2 = output.deviceCoordinateVectex[1];
+        Vector3 p3 = output.deviceCoordinateVectex[2];
+
+        Vector2 vt1 = output.textureVertex[0];
+        Vector2 vt2 = output.textureVertex[1];
+        Vector2 vt3 = output.textureVertex[2];
         PhongShader phongShader = PhongShader(normal, v1, v2, v3);
 
-        float z1 = zPositionInViewCoordinate[indices[i]._1];
-        float z2 = zPositionInViewCoordinate[indices[i]._2];
-        float z3 = zPositionInViewCoordinate[indices[i]._3];
+        float z1 = output.zPositionInViewCoordinate[0];
+        float z2 = output.zPositionInViewCoordinate[1];
+        float z3 = output.zPositionInViewCoordinate[2];
         textureMapping->BindUV(vt1, vt2, vt3, z1, z2, z3);
         Draw::DrawTriangle(bitmapBuffer, p1, p2, p3, phongShader, *textureMapping);
     }
