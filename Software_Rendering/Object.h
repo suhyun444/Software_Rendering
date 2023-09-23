4f47bc6cd21fa5071ad21798286542768abc8aa4
정수현 <windseed71@naver.com>
2023-09-23 20:19:11 +0900

    텍스쳐 uv위치, 텍스쳐 index 추가

--- Software_Rendering/Object.h
+++ Software_Rendering/Object.h
@@ -16,7 +16,9 @@ public:
     std::vector<Vector3> vertices;
     std::vector<Vector3> worldCoordinateVertices;
     std::vector<Vector3> deviceCoordinateVertices;
+    std::vector<Vector2> textureVertices;
     std::vector<Index> indices;
+    std::vector<Index> textureIndices;
     Object();
     void InitVertexVector();
     Matrix4x4 GetModelMatrix();
