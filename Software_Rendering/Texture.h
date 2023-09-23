ee522c8f15695aa92744629a32dc77637cad60a8
정수현 <windseed71@naver.com>
2023-09-23 20:19:12 +0900

    GetPixel 메서드 추가

--- Software_Rendering/Texture.h
+++ Software_Rendering/Texture.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "Window.h"
+#include "math.h"
 
 class Texture
 {
 private:
     HBITMAP hBit;
     BITMAP bitmap;
-
     DWORD *pixels;
     HDC textureHDC;
     HGDIOBJ oldBit;
 public:
+    Texture();
     Texture(std::string name);
     ~Texture();
+    DWORD GetPixel(float x,float y);
 };
