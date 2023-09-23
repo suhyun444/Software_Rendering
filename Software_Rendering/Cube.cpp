287d1be39272f28fc1b33594de5595263e734747
정수현 <windseed71@naver.com>
2023-09-23 20:19:11 +0900

    Cube의 함수 슈퍼클래스로 올리기

--- Software_Rendering/Cube.cpp
+++ Software_Rendering/Cube.cpp
@@ -1,9 +1,6 @@
 #include "Cube.h"
 Cube::Cube()
 {
-	scale = Vector3(1, 1, 1);
-	position = Vector3(0, 0, 0);
-	rotation = Vector3(0, 0, 0);
 
 	vertices.push_back(Vector3(-1, 1, 1));
 	vertices.push_back(Vector3(1, 1, 1));
@@ -30,59 +27,4 @@ Cube::Cube()
 	indices.push_back(Index(1, 4, 5));
 	indices.push_back(Index(7, 6, 5)); 
 	indices.push_back(Index(7, 5, 4));
-}
-
-Matrix4x4 Cube::GetModelMatrix()
-{
-	return Matrix4x4::GetModelMatrix(scale, rotation, position);
-}
-void Cube::UpdateTransform(const Camera& camera)
-{
-	Matrix4x4 modelMatrix = Matrix4x4::GetModelMatrix(scale, rotation, position);
-	Matrix4x4 t = camera.GetProjectionMatrix() * camera.GetViewMatrix() * modelMatrix;
-	for (int i = 0; i < 8; i++)
-	{
-		Vector4 p = Vector4(vertices[i]);
-		
-		Vector4 vWcs = Matrix4x4::GetModelMatrix(scale, rotation, position) * p;
-		worldCoordinateVertices[i] = Vector3(vWcs.x,vWcs.y,vWcs.z);
-		
-		Vector4 vNdcs = t * p;
-		// Clipping coordinate system -> Normalized device coordinate system
-		deviceCoordinateVertices[i] = Vector3(vNdcs.x / vNdcs.w, vNdcs.y / vNdcs.w, vNdcs.z / vNdcs.w);
-		// Normalized device coordinate system -> Device coordinate system
-		deviceCoordinateVertices[i].x += 1;
-		deviceCoordinateVertices[i].x *= (float)Window::width / 2.0f;
-		deviceCoordinateVertices[i].y += 1;
-		deviceCoordinateVertices[i].y *= (float)Window::height / 2.0f;
-	}
-
-}
-// https://velog.io/@jaehyeoksong0/graphics-3
-// TODO : BackfaceCulling 최적화하기
-bool Cube::DontNeedToDraw(Vector3 viewingVector,Vector3 normalVector)
-{
-	return normalVector.Dot(viewingVector) > 0;
-}
-void Cube::Draw(BitmapBuffer &bitmapBuffer, const Camera &camera)
-{
-	Vector3 viewingVector = position - camera.position;
-	for (int i = 0; i < 12; i++)
-	{
-		Vector3 v1 = worldCoordinateVertices[indices[i]._1], v2 = worldCoordinateVertices[indices[i]._2], v3 = worldCoordinateVertices[indices[i]._3];
-		Vector3 s1 = v2 - v1;
-		Vector3 s2 = v3 - v1;
-		Vector3 normal = s1.Cross(s2);
-		normal = normal.Normalize();
-		Vector3 viewingVector = position - camera.position;
-		// back-face culling
-		if (DontNeedToDraw(position - camera.position, normal))	continue;
-
-		Vector3 p1 = deviceCoordinateVertices[indices[i]._1];
-		Vector3 p2 = deviceCoordinateVertices[indices[i]._2];
-		Vector3 p3 = deviceCoordinateVertices[indices[i]._3];
-
-		PhongShader phongShader = PhongShader(normal,v1,v2,v3);
-		Draw::DrawTriangle(bitmapBuffer,p1,p2,p3,phongShader);
-	}
 }
--- Software_Rendering/Cube.h
+++ Software_Rendering/Cube.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "Object.h"
 
-class Cube : Object
+class Cube : public Object
 {
 public:
 	Cube();
-	Matrix4x4 GetModelMatrix();
-	void UpdateTransform(const Camera& camera);
-	bool DontNeedToDraw(Vector3 viewingVector, Vector3 normalVector);
-	void Draw(BitmapBuffer& bitmapBuffer, const Camera& camera);
 };
 
