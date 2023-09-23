9bd23cb9a47d3f074e0492d40cce4b11f619fdf5
정수현 <windseed71@naver.com>
2023-09-23 20:19:11 +0900

    Phong shading완성/ Specular Lighting 구현

--- Software_Rendering/Draw.h
+++ Software_Rendering/Draw.h
@@ -8,7 +8,11 @@ class Draw
 {
 public:
     static float Ambient;
+    static float SpecularStrength;
+    static Vector3 CameraPosition;
     static Vector3 DirectionalLight;
+    static Vector3 GlobalLightColor;
+    static void Update(const Vector3 &cameraPosition) { CameraPosition = cameraPosition; }
     static void PrintText(const HDC& hdc,const int x,const int y, const LPCTSTR& text);
-    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, Vector3 normalVector);
+    static void DrawTriangle(BitmapBuffer &bitmapBuffer, const Vector3 &v1, const Vector3 &v2, const Vector3 &v3, Vector3 normalVector, Vector3 &w1, Vector3 &w2, Vector3 &w3);
 };
