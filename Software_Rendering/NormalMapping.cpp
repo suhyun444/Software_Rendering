3178b44e50dc6158c498b5356f81bb0da6bd37c4
정수현 <windseed71@naver.com>
2023-09-24 21:28:05 +0900

    Add NormalMapping

---
+++ Software_Rendering/NormalMapping.cpp
@@ -0,0 +1,26 @@
+#include "NormalMapping.h"
+
+NormalMapping::NormalMapping()
+{
+}
+NormalMapping::NormalMapping(Texture *texture) : texture(texture)
+{
+}
+
+void NormalMapping::BindUV(const Vector2 &vt1, const Vector2 &vt2, const Vector2 &vt3, float z1, float z2, float z3)
+{
+    this->vt1 = vt1;
+    this->vt2 = vt2;
+    this->vt3 = vt3;
+    this->invZ1 = 1.0f / z1;
+    this->invZ2 = 1.0f / z2;
+    this->invZ3 = 1.0f / z3;
+}
+DWORD NormalMapping::Calcuate(std::pair<float, float> UV)
+{
+    float oneMinusST = 1 - UV.first - UV.second;
+    float z = invZ1 * oneMinusST + invZ2 * UV.first + invZ3 * UV.second;
+    float invZ = 1.0f / z;
+    Vector2 position = (vt1 * oneMinusST * invZ1 + vt2 * UV.first * invZ2 + vt3 * UV.second * invZ3) * invZ;
+    return texture->GetPixel(position.x, position.y);
+}
---
+++ Software_Rendering/NormalMapping.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "math.h"
+#include "Texture.h"
+
+class NormalMapping
+{
+private:
+    Vector2 vt1, vt2, vt3;
+    Texture *texture;
+    float invZ1, invZ2, invZ3;
+
+public:
+    NormalMapping();
+    NormalMapping(Texture *texture);
+    void BindUV(const Vector2 &vt1, const Vector2 &vt2, const Vector2 &vt3, float z1, float z2, float z3);
+    DWORD Calcuate(std::pair<float, float> UV);
+};
